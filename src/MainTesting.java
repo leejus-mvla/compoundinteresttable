290fbadde460ffea6fab96c6bdb4df6c7baae861
justinlee <100027946@mvla.net>
2024-03-06 12:05:45 -0800

    added calculation testing

--- src/MainTesting.java
+++ src/MainTesting.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 public class MainTesting {
 	
+	public static CompoundInterest ci;
+	
 	public static BufferedWriter bw;
 	
 	public static BufferedReader br;
@@ -14,11 +16,17 @@ public class MainTesting {
 	
 	public static int numTest = 1;
 	
+	public static double rate = .05;
+	
+	public static int period = 15;
+	
 	public static void main(String[] args) {
+		ci = new CompoundInterest();
 		f = new File("data/FinGenParams" + numTest + ".txt");
 		fio = new MyFileIO();
-		bw = fio.openBufferedWriter(null);
-		br = fio.openBufferedReader(null);
-		
+		//bw = fio.openBufferedWriter(null);
+		//br = fio.openBufferedReader(null);
+		for (int i = 1; i < period + 1; i++)
+			System.out.println(ci.testingCalc(rate, i) + "\n");
 	}
 }
