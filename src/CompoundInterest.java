2bafc545d9cc76aa3a453f8bc400845b7e476ed4
justinlee <100027946@mvla.net>
2024-03-06 00:21:19 -0800

    added myfileio

--- src/CompoundInterest.java
+++ src/CompoundInterest.java
@@ -1,4 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
 
 public class CompoundInterest {
-
+	
+	private MyFileIO fio;
+	
+	private BufferedReader br;
+	
+	private BufferedWriter bw;
+	
+	public CompoundInterest() {
+		fio = new MyFileIO();
+	}
+	
+	
 }
